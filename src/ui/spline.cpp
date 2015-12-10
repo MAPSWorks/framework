c055008302efbc6bb276f390fc65567ce72dab06
cchen86@stanford.edu <cchen86@stanford.edu>
2015-12-10 12:27:08 -0800

    update spline

--- src/ui/spline.cpp
+++ src/ui/spline.cpp
@@ -3,39 +3,37 @@
 #include "shader.h"
 #include "renderable_object.h"
 
-Spline::Spline(Config conf) 
-: m_config(conf),
-    m_deltaT(0.0f),
-    m_phantomStart(),
-    m_phantomEnd(),
-    m_ptSize(8.0f)
-{
-    m_shader = new Shader("../shader/Spline.vert.glsl", "../shader/Spline.frag.glsl");
-    m_vbo    = new RenderableObject();
-}
+Spline::Spline(Config conf)
+    : m_config(conf),
+      m_deltaT(0.0f),
+      m_phantomStart(),
+      m_phantomEnd(),
+      m_vboLines(new RenderableObject()),
+      m_vboPoints(new RenderableObject()),
+      m_ptSize(8.0f) {}
 
-Spline::~Spline()
-{
-    delete m_shader;
-    delete m_vbo;
-}
+Spline::~Spline() {}
 
-void Spline::addPoint(const glm::vec3 &v)
-{
+void Spline::clear() { m_points.clear(); }
+
+void Spline::addPoint(const glm::vec3 &v) {
     m_points.push_back(v);
-    m_deltaT = (float)1 / ((float)m_points.size()-1);
+    if (m_points.size() <= 1) {
+        return;
+    }
 
-    if(m_points.size() >= 3)
-    {
+    m_deltaT = 1.0f / ((float)m_points.size() - 1);
+
+    // Update m_phantomStart and m_phantomEnd
+    if (m_points.size() >= 3) {
         glm::vec3 sStart = m_points[0];
         glm::vec3 tStart = m_points[1];
 
         glm::vec3 nStart = glm::vec3(sStart - tStart);
         float lenStart = glm::length(nStart);
-        if(lenStart < 1e-5) { 
-            m_phantomStart = sStart; 
-        } 
-        else{
+        if (lenStart < 1e-5) {
+            m_phantomStart = sStart;
+        } else {
             nStart = glm::normalize(nStart);
             glm::vec3 tmp = nStart * lenStart * 0.5f;
             m_phantomStart = sStart + tmp;
@@ -47,116 +45,101 @@ void Spline::addPoint(const glm::vec3 &v)
 
         glm::vec3 nEnd = glm::vec3(sEnd - tEnd);
         float lenEnd = glm::length(nEnd);
-        if(lenEnd < 1e-5) { 
-            m_phantomEnd = sEnd;             
-        } 
-        else{
+        if (lenEnd < 1e-5) {
+            m_phantomEnd = sEnd;
+        } else {
             nEnd = glm::normalize(nEnd);
             glm::vec3 tmp1 = nEnd * lenEnd * 0.5f;
-            m_phantomEnd = sEnd +  tmp1;        
+            m_phantomEnd = sEnd + tmp1;
         }
     }
 }
 
-void Spline::bounds(int &p)
-{
-    if (p < 0) 
-        p = 0; 
+void Spline::bounds(int &p) {
+    if (p < 0)
+        p = 0;
 
-    else if (p >= (int)m_points.size()-1) 
+    else if (p >= (int)m_points.size() - 1)
         p = m_points.size() - 1;
 }
 
-glm::vec3 Spline::interpolatedPoint(float t, Config conf)
-{    
+glm::vec3 Spline::interpolatedPoint(float t, Config conf) {
     // Find out in which interval we are on the spline
     int p = (int)(t / m_deltaT);
 
-    int p0 = p - 1; 
-    int p1 = p;  
+    int p0 = p - 1;
+    int p1 = p;
     int p2 = p + 1;
-    int p3 = p + 2; 
+    int p3 = p + 2;
 
     glm::vec3 c1;
-    if(p0 < 0)
+    if (p0 < 0)
         c1 = m_phantomStart;
     else
         c1 = m_points[p0];
 
     glm::vec3 v1 = m_points[p1];
     glm::vec3 v2;
-    if((uint)p2 >= m_points.size())
-        v2 = m_points[m_points.size()-1];    
+    if ((uint)p2 >= m_points.size())
+        v2 = m_points[m_points.size() - 1];
     else
         v2 = m_points[p2];
 
     glm::vec3 c2;
-    if((uint)p3 >= m_points.size()-1)
+    if ((uint)p3 >= m_points.size() - 1)
         c2 = m_phantomEnd;
     else
         c2 = m_points[p3];
 
-    // Relative (local) time 
+    // Relative (local) time
     float lt = (t - m_deltaT * (float)p) / m_deltaT;
 
-    if(conf == CATMULL_ROM)
+    if (conf == CATMULL_ROM)
         return Spline::catmullRomInterpolation(c1, v1, v2, c2, lt);
-    if(conf == CUBIC)
-        return Spline::bSplineInterpolation(c1, v1, v2, c2, lt);
-    if(conf == BSPLINE)
-        return Spline::cubicInterpolation(c1, v1, v2, c2, lt);
-    if(conf == HERMITE)
+    if (conf == CUBIC) return Spline::bSplineInterpolation(c1, v1, v2, c2, lt);
+    if (conf == BSPLINE) return Spline::cubicInterpolation(c1, v1, v2, c2, lt);
+    if (conf == HERMITE)
         return Spline::hermiteInterpolation(c1, v1, v2, c2, lt);
-    if(conf == KOCHANEK_BARTEL)
+    if (conf == KOCHANEK_BARTEL)
         return Spline::kochanekBartelInterpolation(c1, v1, v2, c2, lt);
-    if(conf == ROUNDED_CATMULL_ROM)
+    if (conf == ROUNDED_CATMULL_ROM)
         return Spline::roundedCatmullRomInterpolation(c1, v1, v2, c2, lt);
 
-    //return LINEAR in default case
+    // return LINEAR in default case
     return Spline::linearInterpolation(v1, v2, lt);
 }
 
-glm::vec3 Spline::point(int n) const
-{
-    return m_points[n];
-}
+glm::vec3 Spline::point(int n) const { return m_points[n]; }
 
-int Spline::numPoints() const
-{
-    return m_points.size();
-}
+int Spline::numPoints() const { return m_points.size(); }
 
-void Spline::render(const Transform &trans, Config conf)
-{
-    /* Small Trick Notice!
-     *  - Use DATA.nx to represent the point size ratio, for example, DATA.nx = 0.5f will be half of the size of m_ptSize so I just need to transfer data once, and call glPointSize once.
-     */
-    std::vector<RenderableObject::Vertex> data;
-    Color pColor(0.7f, 0.7f, 0.7f);
-    //Interpolated Points
-    for(float f=0; f<1.0f; f+= 0.005f/m_points.size())
-    {
+void Spline::render(unique_ptr<Shader> &shader, bool showPoints, Config conf) {
+    if (m_points.size() <= 1) return;
+
+    vector<RenderableObject::Vertex> pointData;
+    vector<RenderableObject::Vertex> lineData;
+
+    glm::vec4 pColor(1.0f, 1.0f, 0.0f, 0.5f);
+    // Interpolated Points
+    for (float f = 0; f <= 1.0f; f += 0.01f) {
         glm::vec3 v = interpolatedPoint(f, conf);
         RenderableObject::Vertex newPt;
         newPt.Position = v;
-        newPt.Normal   = glm::vec3(0.125f, 0.0f, 0.0f);
-        newPt.Color    = glm::vec4(0.7f, 0.7f, 0.7f, 1.0f);
-        data.push_back(newPt);
-    }        
+        newPt.Color = pColor;
+        lineData.push_back(newPt);
+    }
 
-    //Base Points
-    for(uint i=0; i<m_points.size(); ++i)
-    {
+    // Base Points
+    for (uint i = 0; i < m_points.size(); ++i) {
         glm::vec3 v = m_points[i];
         RenderableObject::Vertex newPt;
         newPt.Position = v;
-        newPt.Normal   = glm::vec3(0.25f, 0.0f, 0.0f);
-        data.push_back(newPt);
-    }        
+        newPt.Color = pColor;
+        pointData.push_back(newPt);
+    }
 
-    //Phantom Points
+    // Phantom Points
     std::vector<RenderableObject::Vertex> phantomPoints;
-    Color phantomColor(1.0f, 1.0f, 1.0f);
     float phantomPtSize = 3.0f;
 
     glm::vec3 p1 = m_phantomStart;
@@ -164,86 +147,48 @@ void Spline::render(const Transform &trans, Config conf)
 
     RenderableObject::Vertex newPt1, newPt2;
     newPt1.Position = p1;
-    newPt1.Normal = glm::vec3(0.375f, 0.0f, 0.0f);
+    newPt1.Color = glm::vec4(1.0f, 0.0f, 1.0f, 0.5f);
     newPt2.Position = p2;
-    newPt2.Normal = glm::vec3(0.375f, 0.0f, 0.0f);
-    data.push_back(newPt1);
-    data.push_back(newPt2);
+    newPt2.Color = glm::vec4(0.0f, 1.0f, 1.0f, 0.5f);
+    pointData.push_back(newPt1);
+    pointData.push_back(newPt2);
 
-    //Start/End for Phoantom Points
-    RenderableObject::Vertex sPtStart, sPtEnd, tPtStart, tPtEnd;
-    uint size = m_points.size() - 1;
-    glm::vec3 sStart = m_points[0];
-    glm::vec3 tStart = m_points[1];
-    glm::vec3 sEnd   = m_points[size];
-    glm::vec3 tEnd   = m_points[size-1];   
+    m_vboLines->setData(lineData, GL_LINE_STRIP);
+    m_vboPoints->setData(pointData, GL_POINTS);
 
-    sPtStart.Position = sStart;
-    sPtStart.Normal   = glm::vec3(1.0f, 0.0f, 0.0f);
-    sPtStart.Color    = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
-
-    tPtStart.Position = tStart;
-    tPtStart.Normal   = glm::vec3(1.0f, 0.0f, 0.0f);
-    tPtStart.Color    = glm::vec4(0.5f, 0.0f, 0.0f, 1.0f);
-
-    sPtEnd.Position = sEnd;
-    sPtEnd.Normal   = glm::vec3(1.0f, 0.0f, 0.0f);
-    sPtEnd.Color    = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
-
-    tPtEnd.Position = tEnd;
-    tPtEnd.Normal   = glm::vec3(1.0f, 0.0f, 0.0f);
-    tPtEnd.Color    = glm::vec4(0.0f, 0.0f, 0.5f, 1.0f);
-
-    data.push_back(sPtStart);
-    data.push_back(tPtStart);
-    data.push_back(sPtEnd);
-    data.push_back(tPtEnd);
-
-    m_vbo->setData(data, GL_POINTS);
-
-    glm::mat4 model(1.0f);
-    glm::mat4 view = trans.matView;
-    glm::mat4 projection = trans.matProjection;
-    glm::mat4 viewProjection = trans.matViewProjection;
-
-    m_shader->bind();  
-        m_shader->setMatrix("matModel", model); 
-        m_shader->setMatrix("matView", view);
-        m_shader->setMatrix("matProjection", projection);
-        m_shader->setMatrix("matViewProjection", viewProjection);
-
-        m_vbo->render();
-
-    m_shader->release();
+    if(showPoints) { 
+        m_vboLines->render();
+        glPointSize(m_ptSize);
+        m_vboPoints->render();
+        glPointSize(1.0f);      
+    } 
 }
 
-void Spline::clear()
-{
-    m_points.clear();
+glm::vec3 Spline::linearInterpolation(const glm::vec3 &p0, const glm::vec3 &p1,
+                                      float t) {
+    return p0 * t + (p1 * (1 - t));
 }
 
-glm::vec3 Spline::linearInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, float t)
-{
-    return p0 * t + (p1 * (1-t));
-}
-
-glm::vec3 Spline::catmullRomInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, float t)
-{
+glm::vec3 Spline::catmullRomInterpolation(const glm::vec3 &p0,
+                                          const glm::vec3 &p1,
+                                          const glm::vec3 &p2,
+                                          const glm::vec3 &p3, float t) {
     float t2 = t * t;
     float t3 = t2 * t;
 
-    float b1 = 0.5f * (  -t3 + 2*t2 - t);
-    float b2 = 0.5f * ( 3*t3 - 5*t2 + 2);
-    float b3 = 0.5f * (-3*t3 + 4*t2 + t);
-    float b4 = 0.5f * (   t3 -   t2    );
+    float b1 = 0.5f * (-t3 + 2 * t2 - t);
+    float b2 = 0.5f * (3 * t3 - 5 * t2 + 2);
+    float b3 = 0.5f * (-3 * t3 + 4 * t2 + t);
+    float b4 = 0.5f * (t3 - t2);
 
-    return (p0*b1 + p1*b2 + p2*b3 + p3*b4); 
+    return (p0 * b1 + p1 * b2 + p2 * b3 + p3 * b4);
 }
 
-//not working right
-glm::vec3 Spline::roundedCatmullRomInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, 
-        float t)
-{
+// not working right
+glm::vec3 Spline::roundedCatmullRomInterpolation(const glm::vec3 &p0,
+                                                 const glm::vec3 &p1,
+                                                 const glm::vec3 &p2,
+                                                 const glm::vec3 &p3, float t) {
     glm::vec3 ab = p0 - p1;
     glm::vec3 cb = p2 - p3;
 
@@ -253,7 +198,7 @@ glm::vec3 Spline::roundedCatmullRomInterpolation(const glm::vec3 &p0, const glm:
     bVelo = glm::normalize(bVelo);
 
     glm::vec3 dc = p3 - p2;
-    glm::vec3 bc = -cb;   
+    glm::vec3 bc = -cb;
 
     dc = glm::normalize(dc);
     bc = glm::normalize(bc);
@@ -263,29 +208,32 @@ glm::vec3 Spline::roundedCatmullRomInterpolation(const glm::vec3 &p0, const glm:
 
     float cbDist = glm::length(cb);
 
-    return Spline::catmullRomInterpolation(bVelo * cbDist, p1, p2, cVelo * cbDist, t);
+    return Spline::catmullRomInterpolation(bVelo * cbDist, p1, p2,
+                                           cVelo * cbDist, t);
 }
 
-glm::vec3 Spline::cubicInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, float t)
-{
+glm::vec3 Spline::cubicInterpolation(const glm::vec3 &p0, const glm::vec3 &p1,
+                                     const glm::vec3 &p2, const glm::vec3 &p3,
+                                     float t) {
     float t2;
     glm::vec3 a0, a1, a2, a3;
 
-    t2 = t*t;
+    t2 = t * t;
     a0 = p3 - p2 - p0 + p1;
     a1 = p0 - p1 - a0;
     a2 = p2 - p0;
     a3 = p1;
 
     float tmp1 = t * t2;
-    glm::vec3 res = a0*tmp1;
+    glm::vec3 res = a0 * tmp1;
 
-    return(a0*tmp1 + a1*t2 + a2*t + a3);
+    return (a0 * tmp1 + a1 * t2 + a2 * t + a3);
 }
 
-//not working right
-glm::vec3 Spline::bSplineInterpolation(const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, const glm::vec3 &p4, float t)
-{
+// not working right
+glm::vec3 Spline::bSplineInterpolation(const glm::vec3 &p1, const glm::vec3 &p2,
+                                       const glm::vec3 &p3, const glm::vec3 &p4,
+                                       float t) {
     glm::vec4 a, b, c;
 
     a.x = (-p1.x + 3 * p2.x - 3 * p3.x + p4.x) / 6.0;
@@ -303,72 +251,72 @@ glm::vec3 Spline::bSplineInterpolation(const glm::vec3 &p1, const glm::vec3 &p2,
     c.z = (-3 * p1.y + 3 * p3.y) / 6.0;
     c.w = (p1.y + 4 * p2.y + p3.y) / 6.0;
 
-    glm::vec3 p = glm::vec3( ((a.z + t * (a.y + t * a.x))*t+a.w), ((b.z + t * (b.y + t * b.x))*t+b.w), ((c.z + t * (c.y + t * c.x))*t+c.w) ) ;
+    glm::vec3 p = glm::vec3(((a.z + t * (a.y + t * a.x)) * t + a.w),
+                            ((b.z + t * (b.y + t * b.x)) * t + b.w),
+                            ((c.z + t * (c.y + t * c.x)) * t + c.w));
 
     return p;
-
 }
 
-glm::vec3 Spline::hermiteInterpolation(const glm::vec3 &p0, 
-        const glm::vec3 &p1, 
-        const glm::vec3 &p2, 
-        const glm::vec3 &p3, 
-        float t, 
-        float tension, 
-        float bias)
-{
+glm::vec3 Spline::hermiteInterpolation(const glm::vec3 &p0, const glm::vec3 &p1,
+                                       const glm::vec3 &p2, const glm::vec3 &p3,
+                                       float t, float tension, float bias) {
     glm::vec3 m0, m1;
-    float t2,t3;
+    float t2, t3;
     float a0, a1, a2, a3;
 
     t2 = t * t;
     t3 = t2 * t;
 
-    m0  = (p1-p0)*(1.0f+bias)*(1.0f-tension)/2.0f;
-    m0 += (p2-p1)*(1.0f-bias)*(1.0f-tension)/2.0f;
-    m1  = (p2-p1)*(1.0f+bias)*(1.0f-tension)/2.0f;
-    m1 += (p3-p2)*(1.0f-bias)*(1.0f-tension)/2.0f;
+    m0 = (p1 - p0) * (1.0f + bias) * (1.0f - tension) / 2.0f;
+    m0 += (p2 - p1) * (1.0f - bias) * (1.0f - tension) / 2.0f;
+    m1 = (p2 - p1) * (1.0f + bias) * (1.0f - tension) / 2.0f;
+    m1 += (p3 - p2) * (1.0f - bias) * (1.0f - tension) / 2.0f;
 
-    a0 =  2*t3 - 3*t2 + 1;
-    a1 =    t3 - 2*t2 + t;
-    a2 =    t3 -   t2;
-    a3 = -2*t3 + 3*t2;
+    a0 = 2 * t3 - 3 * t2 + 1;
+    a1 = t3 - 2 * t2 + t;
+    a2 = t3 - t2;
+    a3 = -2 * t3 + 3 * t2;
 
-    return(a0*p1 + a1*m0 + a2*m1 + a3*p2);
+    return (a0 * p1 + a1 * m0 + a2 * m1 + a3 * p2);
 }
 
-glm::vec3 Spline::picewiseHermiteInterpolation(const glm::vec3 &a, const glm::vec3 &b, const glm::vec3 &startTangent, 
-        const glm::vec3 &endTangent, float t)
-{
+glm::vec3 Spline::picewiseHermiteInterpolation(const glm::vec3 &a,
+                                               const glm::vec3 &b,
+                                               const glm::vec3 &startTangent,
+                                               const glm::vec3 &endTangent,
+                                               float t) {
     float t2, t3;
     float a0, a1, b0, b1;
 
-    t2 = t*t;
-    t3 = t2*t;
+    t2 = t * t;
+    t3 = t2 * t;
 
     a0 = (t3 * 2.0f) - (3.0f * t2) + 1.0f;
-    a1 = (-2.0f * t3) + (3.0f * t2);    
+    a1 = (-2.0f * t3) + (3.0f * t2);
     b0 = t3 - (2.0f * t2) + t;
-    b1 = t3 -   t2;
+    b1 = t3 - t2;
 
-    return (a0*a + a1*b + b0*startTangent + b1*endTangent);
+    return (a0 * a + a1 * b + b0 * startTangent + b1 * endTangent);
 }
 
-//not working right
-glm::vec3 Spline::kochanekBartelInterpolation(const glm::vec3 &a, const glm::vec3 &b, const glm::vec3 &c, const glm::vec3 &d, 
-        float t, float tension, float bias, float continuity)
-{
+// not working right
+glm::vec3 Spline::kochanekBartelInterpolation(
+    const glm::vec3 &a, const glm::vec3 &b, const glm::vec3 &c,
+    const glm::vec3 &d, float t, float tension, float bias, float continuity) {
     glm::vec3 ab = b - a;
     ab = glm::normalize(ab);
     glm::vec3 cd = d - c;
     cd = glm::normalize(cd);
 
-    glm::vec3 inTangent = ((1.0f - tension) * (1.0f - continuity) * (1.0f + bias)) * 0.5f * ab 
-        + ((1.0f - tension) * (1.0f + continuity) * (1.0f - bias)) * 0.5f * cd;
+    glm::vec3 inTangent =
+        ((1.0f - tension) * (1.0f - continuity) * (1.0f + bias)) * 0.5f * ab +
+        ((1.0f - tension) * (1.0f + continuity) * (1.0f - bias)) * 0.5f * cd;
 
-    glm::vec3 outTangent = ((1.0f - tension) * (1.0f + continuity) * (1.0f + bias)) * 0.5f * ab
-        + ((1.0f - tension) * (1.0f - continuity) * (1.0f - bias)) * 0.5f * cd;
+    glm::vec3 outTangent =
+        ((1.0f - tension) * (1.0f + continuity) * (1.0f + bias)) * 0.5f * ab +
+        ((1.0f - tension) * (1.0f - continuity) * (1.0f - bias)) * 0.5f * cd;
 
-    //return picewiseHermiteInterpolation(b, c, inTangent, outTangent, t);
+    // return picewiseHermiteInterpolation(b, c, inTangent, outTangent, t);
     return hermiteInterpolation(inTangent, b, c, outTangent, t);
 }
--- src/ui/spline.h
+++ src/ui/spline.h
@@ -13,54 +13,78 @@ Created by Chen Chen on 09/28/2015
 class Shader;
 class RenderableObject;
 
-class Spline
-{
-    public:
-        enum Config
-        {
-            CATMULL_ROM = 0,
-            CUBIC, 
-            HERMITE,
-            PICEWISE_HERMITE,
-            COSINE,
-            LINEAR,
-            KOCHANEK_BARTEL,
-            ROUNDED_CATMULL_ROM,
-            BSPLINE
-        };
+class Spline {
+public:
+    enum Config {
+        CATMULL_ROM = 0,
+        CUBIC,
+        HERMITE,
+        PICEWISE_HERMITE,
+        COSINE,
+        LINEAR,
+        KOCHANEK_BARTEL,
+        ROUNDED_CATMULL_ROM,
+        BSPLINE
+    };
 
-        Spline(Config conf = CATMULL_ROM);
-        ~Spline();
+    Spline(Config conf = CATMULL_ROM);
+    ~Spline();
 
-        void addPoint(const glm::vec3 &v);
-        void clear();
-        glm::vec3 interpolatedPoint(float t, Config conf = CATMULL_ROM);
-        glm::vec3 point(int n) const;
-        int numPoints() const;
-        void render(const Transform &trans, Config conf = CATMULL_ROM);
+    void clear();
 
-        void bounds(int &p);
+    void addPoint(const glm::vec3 &v);
+    void bounds(int &p);
 
-        static glm::vec3 linearInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, float t);
-        static glm::vec3 catmullRomInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, float t);
-        static glm::vec3 roundedCatmullRomInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, float t);
-        static glm::vec3 cubicInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, float t);
-        static glm::vec3 bSplineInterpolation(const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, const glm::vec3 &p4, float t);
-        static glm::vec3 hermiteInterpolation(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, float t, float tension = 0.0, float bias = 0.0);
-        static glm::vec3 picewiseHermiteInterpolation(const glm::vec3 &a, const glm::vec3 &b, const glm::vec3 &startTangent, const glm::vec3 &endTangent, float t);
-        static glm::vec3 kochanekBartelInterpolation(const glm::vec3 &a, const glm::vec3 &b, const glm::vec3 &c, const glm::vec3 &d, float t, float tension = 0.0, float bias = 0.0, float continuity = 0.0f);
+    glm::vec3 interpolatedPoint(float t, Config conf = CATMULL_ROM);
+    glm::vec3 point(int n) const;
+    int numPoints() const;
+    void render(unique_ptr<Shader> &shader, bool showPoints = true,
+                Config conf = CATMULL_ROM);
 
-        vector<glm::vec3> m_points;
-        glm::vec3 m_phantomStart;
-        glm::vec3 m_phantomEnd;
+    static glm::vec3 linearInterpolation(const glm::vec3 &p0,
+                                         const glm::vec3 &p1, float t);
+    static glm::vec3 catmullRomInterpolation(const glm::vec3 &p0,
+                                             const glm::vec3 &p1,
+                                             const glm::vec3 &p2,
+                                             const glm::vec3 &p3, float t);
+    static glm::vec3 roundedCatmullRomInterpolation(const glm::vec3 &p0,
+                                                    const glm::vec3 &p1,
+                                                    const glm::vec3 &p2,
+                                                    const glm::vec3 &p3,
+                                                    float t);
+    static glm::vec3 cubicInterpolation(const glm::vec3 &p0,
+                                        const glm::vec3 &p1,
+                                        const glm::vec3 &p2,
+                                        const glm::vec3 &p3, float t);
+    static glm::vec3 bSplineInterpolation(const glm::vec3 &p1,
+                                          const glm::vec3 &p2,
+                                          const glm::vec3 &p3,
+                                          const glm::vec3 &p4, float t);
+    static glm::vec3 hermiteInterpolation(
+        const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2,
+        const glm::vec3 &p3, float t, float tension = 0.0, float bias = 0.0);
+    static glm::vec3 picewiseHermiteInterpolation(const glm::vec3 &a,
+                                                  const glm::vec3 &b,
+                                                  const glm::vec3 &startTangent,
+                                                  const glm::vec3 &endTangent,
+                                                  float t);
+    static glm::vec3 kochanekBartelInterpolation(
+        const glm::vec3 &a, const glm::vec3 &b, const glm::vec3 &c,
+        const glm::vec3 &d, float t, float tension = 0.0, float bias = 0.0,
+        float continuity = 0.0f);
 
-    private:    
-        float m_deltaT;
-        Config m_config;
+    vector<glm::vec3> m_points;
+    glm::vec3 m_phantomStart;
+    glm::vec3 m_phantomEnd;
 
-        Shader                    *m_shader;
-        RenderableObject *m_vbo;
-        float                     m_ptSize;
+private:
+    Config m_config;
+
+    float m_deltaT;  // = 1.0 / (m_points.size() - 1)
+    float m_ptSize;
+
+    unique_ptr<RenderableObject> m_vboPoints;
+    unique_ptr<RenderableObject> m_vboLines;
 };
 
 #endif /* end of include guard: SPLINE_H_ */
